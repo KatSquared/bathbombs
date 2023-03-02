67098c92f45822f0f99956bb0ab07f6bbcaa67be
Katarzyna Kmita <kmita.katarzyna.a@gmail.com>
2023-03-02 19:08:22 +0100

    sidebar menu fix

--- src/menu.js
+++
@@ -1,9 +0,0 @@
-const menuLinks = document.getElementById("menuLinks");
-
-  function showMenu() {
-      menuLinks.style.right = "0";
-  }
-
-  function hideMenu() {
-      menuLinks.style.right = "-400px";
-  }
--- src/navbar-and-footer-components.js
+++ src/navbar-and-footer-components.js
@@ -265,3 +265,16 @@ let scrollToContact = () => {
     block: 'start'
   });
 }
+
+///
+// MENU SIDEBAR SLIDE 
+//
+const menuLinks = document.getElementById("menuLinks");
+
+  function showMenu() {
+      menuLinks.style.right = "0";
+  }
+
+  function hideMenu() {
+      menuLinks.style.right = "-400px";
+  }
